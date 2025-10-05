f041b2dd98cfc5b259832f85fdf394e12adfe76f
jorgensd <dokken92@gmail.com>
2025-10-05 10:56:27 +0200

    Update intro py

--- intro.py
+++ intro.py
@@ -1,19 +1,9 @@
 # # Welcome to your Jupyter Book
 
 
-def foo():
-    """Some function.
-
-    .. pyvista-plot::
-
-        >>> import pyvista as pv
-        >>> mesh = pv.Sphere()
-        >>> mesh.plot()
-    """
-    pass
-
-
 import pyvista as pv
 
 mesh = pv.Sphere()
 mesh.plot()
+
+print("HLLOssss")
